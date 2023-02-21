e7d48562f7d3d2180496e5ea8609e04bf8b0601f
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-02-20 20:36:38 -0600

    refactor(cargo): Define common manifest values in workspace block

--- rust-crane/crates/cli/src/main.rs
+++ rust-crane/crates/cli/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use crate::commands::{Commands, HelloCommands};
+
+mod commands;
+
+use clap::Parser;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt::init();
+
+    let args = commands::Args::parse();
+    match args.command {
+        Commands::Hello(hello) => {
+            let cmd = hello.command;
+            match cmd {
+                HelloCommands::World => {
+                    println!("Hello, world!");
+                }
+                HelloCommands::Name { name } => {
+                    println!("Hello, {}!", name);
+                }
+            }
+        }
+    }
+
+    Ok(())
 }
+
